44f91ee94e482a910391f56bdaf2f4ac823045d0
M Stefan Walker <m.stefan.walker@gmail.com>
2026-04-01 01:48:11 -0400

    Add plants

---
+++ simulation.js
@@ -0,0 +1,124 @@
+sim = {
+
+    color: '#8da',
+    border: 0.2, // 0 to 0.5
+    density: 0.0014, // plants per pixel
+    plant: {min: 100, range: 1000}, // birth age
+    child: {min: 60, range: 10000}, // birth age
+    length: {pow: 0.8, scale: 0.2},
+    width: {min: 2, pow: 0.4, scale: 0.2},
+    generations: 6,
+
+    stop: () => {},
+    model: {},
+    canvas: null,
+    context: null,
+
+    run: function() {
+        sim.init()
+        sim.start()
+    },
+
+    init: function() {
+        // display
+        sim.canvas = document.getElementById('simulation')
+        sim.canvas.width = window.innerWidth
+        sim.canvas.height = window.innerHeight
+        sim.context = sim.canvas.getContext('2d')
+
+        // model
+        sim.model = {}
+        let numPlants = Math.ceil(window.innerWidth * sim.density)
+        let plants = []
+        for (let i = 0; i < numPlants; i++) {
+            let angle = -Math.PI/2
+            let left = sim.canvas.width * sim.border
+            let range = sim.canvas.width - (sim.canvas.width * sim.border * 2)
+            plants.push({
+                root: [
+                    left + Math.random() * range,
+                    sim.canvas.height
+                ],
+                generation: 0,
+                angle: angle,
+                age: 0,
+                birthAge: sim.plant.min + (Math.floor(Math.random() * sim.plant.range)),
+                children: [] // [{direction, age}, {...}, ...]
+            })
+        }
+        sim.model.plants = plants
+    },
+
+    start: function() {
+        sim.stop()
+        let id = setInterval(() => {
+            sim.step()
+            sim.display()
+        }, 40)
+        sim.stop = () => window.clearInterval(id)
+    },
+
+    step: function(num = 1) {
+        if (num !== 1) for (let i = 0; i <= num; i++) sim.step(1)
+        function birthAge() {
+            return sim.child.min + (Math.floor(Math.random() * sim.child.range))
+        }
+        function agePart(part) {
+            part.age++
+            if (part.age === part.birthAge && part.generation <= sim.generations) {
+                part.children = [
+                    {
+                        generation: part.generation+1,
+                        angle: part.angle + Math.random(),
+                        age: 0,
+                        birthAge: birthAge(),
+                        children: [] // [{direction, age}, {...}, ...]
+                    },
+                    {
+                        generation: part.generation+1,
+                        angle: part.angle - Math.random(),
+                        age: 0,
+                        birthAge: birthAge(),
+                        children: [] // [{direction, age}, {...}, ...]
+                    }
+                ]
+            }
+            if (part.children.length === 0) return
+            part.children.forEach(child => {
+                agePart(child)
+            })
+        }
+        sim.model.plants.forEach(plant => agePart(plant))
+    },
+
+    display: function() {
+        function drawPart(part, start) {
+            let angle = part.angle
+            let age = part.age
+            let length = Math.pow(age, sim.length.pow) * sim.length.scale
+            let width = sim.width.min + Math.pow(age, sim.width.pow) * sim.width.scale
+            let end = [
+                start[0] + (Math.cos(angle) * length),
+                start[1] + (Math.sin(angle) * length),
+            ]
+            sim.context.lineWidth = width
+            sim.context.beginPath()
+            sim.context.moveTo(start[0], start[1])
+            sim.context.lineTo(end[0], end[1])
+            sim.context.stroke()
+            if (part.children.length === 0) return
+            part.children.forEach(child => {
+                drawPart(child, end)
+            })
+        }
+        sim.context.clearRect(0, 0, sim.canvas.width, sim.canvas.height)
+        sim.context.lineCap = 'round'
+        sim.context.strokeStyle = sim.color
+        sim.model.plants.forEach(plant => {
+            drawPart(plant, plant.root)
+        })
+    },
+}
+
+document.addEventListener('DOMContentLoaded', sim.run)
+window.addEventListener('resize', sim.init)
